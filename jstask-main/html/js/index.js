744d585eb7a3e5f1d6a404f4e2f7a09653ac4425
TIRATH-PATHAK <86818390+TIRATH-PATHAK@users.noreply.github.com>
2021-09-09 23:37:53 +0530

    Add files via upload

--- jstask-main/html/js/index.js
+++ jstask-main/html/js/index.js
@@ -3,29 +3,32 @@ var x = document.getElementsByClassName('texts');
 
 function btn(number) {
      var z = document.getElementById(number);
+     
      if (z.classList.contains("hide")) {
-
-
-         
           for (let i = 0; i < x.length; i++) {
+              
               x[i].classList.add("hide")
           }
+          
           z.classList.remove("hide")
           
       } else{
   
           z.classList.add("hide")
-  
-         
+   
       }
 }
 
 
 var acc = document.getElementsByClassName('button');
-
+console.log(acc);
 for(i = 0;i<acc.length;i++){
     acc[i].addEventListener('click',function(){
+        console.log(this.classList);
         this.classList.toggle('active');
         
+
     });
+
+   
 }
